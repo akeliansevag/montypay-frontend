9989aac8c7a1debf0786d7fd03483e8fd78fba77
montyholdingdev <155431830+montyholdingdev@users.noreply.github.com>
2024-02-21 14:42:35 +0200

    .

---
+++ pages/faq/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <Hero breadcrumbs="Solutions / Tech Services" title="FAQ"
+        paragraph="MontyPay provides an End-to-End Payment Solution for Merchants" button="Get Started"
+        image="white-label" />
+</template>
+
+<script setup>
+useSeoMeta({
+    title: 'FAQ',
+    ogTitle: 'FAQ',
+    description: 'FAQ description',
+    ogDescription: 'FAQ description',
+    ogImage: 'https://example.com/image.png',
+    twitterCard: 'summary_large_image',
+})
+</script>
+
+<style lang="scss" scoped></style>
