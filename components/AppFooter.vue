38330e3694919efff0d13a03cf23f4b5366296ce
Sevag Akelian <akelian.sevag@gmail.com>
2024-04-11 23:48:30 +0300

    .

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -31,7 +31,7 @@
                 </div>
             </div>
 
-            <hr class="border-[#e9edf422] mt-10" />
+            <!-- <hr class="border-[#e9edf422] mt-10" />
 
             <nav class="flex flex-col lg:flex-row flex-wrap gap-2.5 lg:gap-9 mt-10">
                 <NavItem to="/terms-and-conditions" label="Terms & Conditions" layout="footer" class="flex-none" />
@@ -41,7 +41,7 @@
                 <NavItem to="/platform-services-agreement" label="Platform Services Agreement" layout="footer" class="flex-none" />
             </nav>
 
-            <hr class="border-[#e9edf422] mt-10" />
+            <hr class="border-[#e9edf422] mt-10" /> -->
 
             <div class="flex flex-col-reverse lg:flex-row items-center justify-between gap-10 mt-10">
                 <p>© MontyPay 2024</p>
