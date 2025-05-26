221f9b09ffc4a8938c9663340bcd1fb782e74044
akeliansevag <akelian.sevag@gmail.com>
2025-05-26 14:08:43 +0300

    .

---
+++ components/FadeSlider.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="container">
+        <h2 v-if="title" class="text-2xl">{{ title }}</h2>
+        <div :class="reverse && 'flex-row-reverse'" class="flex mt-10 rounded-xl overflow-hidden max-lg:flex-col  lg:h-[500px]">
+            <div class="lg:w-1/2 bg-gradient-to-r from-[#E6EBF3] to-[#E6F1F6] max-lg:py-10 lg:py-16">
+                <div v-if="data" class="flex flex-col justify-center h-full">
+                    <div class="relative">
+                        <Transition name="fade" mode="out-in">
+                            <div
+                                v-if="data[selectedIndex]"
+                                :key="selectedIndex"
+                                class="lg:px-16 max-lg:px-10"
+                            >
+                                <div>
+                                    <h4 class="mb-1 text-xl font-bold">{{ data[selectedIndex].title }}</h4>
+                                    <p>{{ data[selectedIndex].description }}</p>
+                                </div>
+                            </div>
+                        </Transition>
+                        <div class="flex gap-5 max-lg:mt-10 lg:mt-20 max-lg:flex-wrap px-10">
+                            <button @click="handleClick(index)" v-for="(item,index) in data" :key="index" class="filter-button" :class="selectedIndex===index && 'active'">
+                                {{ item.category }}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="lg:w-1/2 bg-gradient-to-r from-[#D2EAF1] to-[#E7F2F7] max-lg:px-10 lg:px-16 max-lg:py-10 lg:py-16 overflow-hidden">
+                <div v-if="data" class="w-full h-full relative">
+                    <Transition name="fade" mpde="out-in">
+                        <div class="lg:top-1/2 lg:-translate-y-1/2" v-if="data[selectedIndex]" :key="selectedIndex">
+                            <img class="mx-auto object-contain" :src="data[selectedIndex].image" :alt="data[selectedIndex].title" />
+                        </div> 
+                    </Transition>
+                                                          
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    const selectedIndex = ref(0);
+    const props = defineProps(['reverse','title','data']);
+
+    const handleClick = (index) => {
+        selectedIndex.value = index
+    }
+</script>
+
+<style lang="sass" scoped>
+.filter-button
+  @apply text-[#233F94] font-bold text-center py-2 border rounded-full border-[#233F94] bg-transparent lg:w-full px-4
+
+  &.active, &:hover
+    @apply text-white bg-gradient-to-r from-[#013383] to-[#01B0C6] border border-white
+.fade-leave-active
+    transition: none
+.fade-enter-active
+  transition: opacity 1s ease
+
+.fade-enter-from, .fade-leave-to
+  opacity: 0
+</style>
---
+++ pages/solutions/q-check/index.vue
@@ -0,0 +1,88 @@
+<template>
+    <section class="relative">
+
+        <img src="/pattern.svg" alt="pattern" class="max-lg:hidden absolute -z-10 right-0" />
+
+        <div class="container">
+            <div class="flex max-lg:flex-col max-lg:gap-1 lg:justify-between">
+                <div class="lg:w-[50%] flex flex-col gap-7 my-10">
+                    <h1>
+                        QCheck: The Self-Service Checkout Solution That Works for You
+                    </h1>
+                    <p>
+                        Flip the floor faster and keep them coming back with QCheck, the ultimate self-service checkout
+                        tool. Whether guests are scanning to browse your menu
+                        or settling their bill, QCheck is designed for speed, efficiency, and satisfaction.
+                    </p>
+                    <div>
+                        <div class="relative">
+                            <input required type="text" placeholder="What's your restaurant's name?"
+                                class="border border-[#D9D9D9] w-full rounded-xl px-5 py-4 lg:pr-[170px] shadow-md" />
+                            <button
+                                class="mp-button-quad lg:absolute max-lg:mt-1 max-lg:w-full lg:right-1 lg:h-[85%] lg:top-1/2 lg:-translate-y-1/2">
+                                Request a Demo
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="lg:w-[35%]">
+                    <img src="/qcheck-banner.webp" alt="QCheck Banner Image" />
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <FadeSlider :data="sliderOne" title="Starts With a Scan. Ends With a Smile."/>
+    <div class="container">
+        <div class="separator"></div>
+    </div>
+
+    <FadeSlider title="It’s All Just One Scan Away " reverse="true"/>
+    <div class="container">
+        <div class="separator"></div>
+    </div>
+    
+</template>
+
+<script setup>
+useSeoMeta({
+    title: 'QCheck',
+    ogTitle: 'QCheck',
+    description: 'QCheck ',
+    ogDescription: 'QCheck ',
+    ogImage: 'https://example.com/image.png',
+    twitterCard: 'summary_large_image',
+})
+
+const sliderOne = [
+    {
+        title: 'Faster Checkout',
+        description: 'Save up to 17 minutes per table for faster turnover with QCheck\'s seamless pay-at-table experience.',
+        image: '/faster-checkout.webp',
+        category: 'Faster',
+    },
+    {
+        title: 'Seamless Experience',
+        description: 'Ensure a frictionless journey from menu browsing to bill payment, leaving guests happy and operations smooth.',
+        image: '/seamless-experience.webp',
+        category: 'Seamless',
+    },
+    {
+        title: 'Simple and Secure Payments',
+        description: 'Offer confidence and peace of mind with multiple secure payment options, from mobile wallets to cards.',
+        image: '/simple-and-secure-payments.webp',
+        category: 'Secure',
+    },
+    {
+        title: 'Multiple Payment Options',
+        description: 'Give guests flexibility with wallet payments, card payments, and many more, all in one hassle-free process',
+        image: '/multiple-payment-options.webp',
+        category: 'Flexible',
+    },
+
+
+];
+</script>
+
+<style lang="scss" scoped></style>
