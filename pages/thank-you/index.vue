a996bd96e24cc19b4ce69e792da9cff30c7332f1
akeliansevag <akelian.sevag@gmail.com>
2025-11-26 11:44:11 +0200

    .

--- pages/thank-you/index.vue
+++ pages/thank-you/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="bg-white lg:py-32 max-lg:py-16">
+    <section class="bg-white lg:py-52 max-lg:py-16">
         <div class="container mx-auto px-4 text-center">
             <h1 class="text-4xl font-bold mb-4">Thank You for Reaching Out.</h1>
             <p class="text-lg text-gray-700">Our team is already reviewing your request, and we’ll get back to you as soon as possible.</p>
