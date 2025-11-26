6cfb26653bf34c2aff356032e4684b924f3f6562
akeliansevag <akelian.sevag@gmail.com>
2025-11-26 11:34:07 +0200

    thank you page

---
+++ pages/thank-you/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="bg-white lg:py-32 max-lg:py-16">
+        <div class="container mx-auto px-4 text-center">
+            <h1 class="text-4xl font-bold mb-4">Thank You for Reaching Out.</h1>
+            <p class="text-lg text-gray-700">Our team is already reviewing your request, and we’ll get back to you as soon as possible.</p>
+            <NuxtLink to="/" class="mp-button-quaternary mt-5 inline-block">Return to Home</NuxtLink>
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
