d6aa2617215abbc4a7032ca9d5b1fcc5e5a0b076
Hassan Jammal <hassan-jammal@hotmail.com>
2026-04-27 16:32:09 +0300

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -50,14 +50,12 @@ export default defineNuxtConfig({
       script: [
         // ✅ Google Tag Manager
         {
-          hid: 'gtm',
+          key: 'gtm',
           innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-5GF46VB3');`,
-          type: 'text/javascript',
-          charset: 'utf-8'
         },
         {
           hid: 'snap-pixel',
@@ -94,14 +92,12 @@ export default defineNuxtConfig({
       // ✅ GTM noscript iframe in body
       noscript: [
         {
-          hid: 'gtm-noscript',
+          key: 'gtm-noscript',
           innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5GF46VB3" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-          pbody: true  // places it at the top of <body>
+          tagPosition: 'bodyOpen'
         }
       ],
       __dangerouslyDisableSanitizersByTagID: {
-        'gtm': ['innerHTML'],
-        'gtm-noscript': ['innerHTML'],
         'snap-pixel': ['innerHTML'],
         'metricool': ['innerHTML'],
       }
